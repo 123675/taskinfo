13c7819e22932d098308488b78062a2b7e1facfe
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-18 22:19:36 +0000

    reduce learning rate

--- centroid-networks/shell/miniimagenet_fewshot/run_miniimagenet_unsupervised.sh
+++ centroid-networks/shell/miniimagenet_fewshot/run_miniimagenet_unsupervised.sh
@@ -9,4 +9,4 @@ if [ -n ${RELOAD} ]; then RELOAD_CMD="--checkpoint-state ${PATH_TO_PRETRAINED}";
 # Replace this accordingly
 echo "CUDA visible devices $CUDA_VISIBLE_DEVICES"
 TRAINLOSS=$1  # pairwiseunsupervised, pairwisesupervised, pairwisecombined
-PYTHONPATH=../.. python ../../scripts/train/few_shot/run_train.py --data.dataset miniimagenet --data.root $HOME/data/miniimagenet --model.model_name "pairwise_conv" --log.exp_dir results/miniimagenet5-$TRAINLOSS --train-loss $TRAINLOSS --centroid-loss 0 --regularization 1 --iterations 100000 --data.way 5 --data.test_way 5 --data.cuda ${RELOAD_CMD}
+PYTHONPATH=../.. python ../../scripts/train/few_shot/run_train.py --data.dataset miniimagenet --data.root $HOME/data/miniimagenet --model.model_name "pairwise_conv" --log.exp_dir results/miniimagenet5-$TRAINLOSS --train-loss $TRAINLOSS --centroid-loss 0 --regularization 1 --iterations 100000 --data.way 5 --data.test_way 5 --data.cuda ${RELOAD_CMD} --train.learning_rate 0.0001
--- centroid-networks/shell/monitor.sh
+++ centroid-networks/shell/monitor.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 FOLDER=$1
 SPLIT=${2:-val}
-echo "Monitoring $FOLDER"
+MODE=${3:-adjusted}
+
+if [ "$MODE" != "adjusted" ] && [ "$MODE" != "pairwise" ]; then echo "MODE must be adjusted or pairwise"; exit 1; fi
+
+echo "~"
+echo "    < $FOLDER >    "
 ((a = `date +%s`))
 ((b = `date -r $FOLDER/summary.txt +%s`))
-echo "Refreshed $(( (a-b) / (60))) minutes ago"
-date -r $FOLDER/summary.txt
+echo "(refreshed $(( (a-b) / (60))) minutes ago)"
+#date -r $FOLDER/summary.txt
 cat $FOLDER/summary.txt|grep "$SPLIT/SupervisedAcc_softmax"
-cat $FOLDER/summary.txt|grep "$SPLIT/AdjustedAcc_supervised"
-cat $FOLDER/summary.txt|grep "$SPLIT/AdjustedAcc_unsupervised"
-cat $FOLDER/summary.txt|grep "$SPLIT/AdjustedAcc_supervised"
-cat $FOLDER/summary.txt|grep "$SPLIT/AdjustedLoss_unsupervised"
+
+if [ "$MODE" = "adjusted" ]
+then
+	cat $FOLDER/summary.txt|grep "$SPLIT/AdjustedAcc_supervised"
+	cat $FOLDER/summary.txt|grep "$SPLIT/AdjustedAcc_unsupervised"
+	cat $FOLDER/summary.txt|grep "$SPLIT/AdjustedLoss_supervised"
+	cat $FOLDER/summary.txt|grep "$SPLIT/AdjustedLoss_unsupervised"
+else
+	cat $FOLDER/summary.txt|grep "$SPLIT/PairwiseAcc_supervised"
+	cat $FOLDER/summary.txt|grep "$SPLIT/PairwiseAcc_unsupervised"
+	cat $FOLDER/summary.txt|grep "$SPLIT/PairwiseLoss_supervised"
+	cat $FOLDER/summary.txt|grep "$SPLIT/PairwiseLoss_unsupervised"
+fi
+
