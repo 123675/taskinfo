9c509d554ce2cabc4f8ebec7e68683ecc2978a42
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-19 04:21:06 +0000

    fix reload cmd

--- centroid-networks/taskinfo/run_MODE_TRAINLOSS_DATASET_WAYS_SHOTS_RELOAD.sh
+++ centroid-networks/taskinfo/run_MODE_TRAINLOSS_DATASET_WAYS_SHOTS_RELOAD.sh
@@ -17,4 +17,4 @@ if [ -n ${RELOAD} ]; then RELOAD_CMD="--checkpoint-state ${PATH_TO_PRETRAINED}";
 echo "CUDA visible devices $CUDA_VISIBLE_DEVICES"
 set -o xtrace
 
-PYTHONPATH=.. python ../scripts/train/few_shot/run_train.py --data.dataset ${DATASET} --data.root $HOME/data/${DATASET} --model.model_name "pairwise_conv" --log.exp_dir results/${DATASET}-${WAYS}way-${SHOTS}shot-$TRAINLOSS --train-loss "${ACTUALTRAINLOSS}" --centroid-loss 0 --validate-interval 5 --iterations 100000 --data.way ${WAYS} --data.test_way ${WAYS} --train.learning_rate 0.0001 --data.cuda
+PYTHONPATH=.. python ../scripts/train/few_shot/run_train.py --data.dataset ${DATASET} --data.root $HOME/data/${DATASET} --model.model_name "pairwise_conv" --log.exp_dir results/${DATASET}-${WAYS}way-${SHOTS}shot-$TRAINLOSS --train-loss "${ACTUALTRAINLOSS}" --centroid-loss 0 --validate-interval 5 --iterations 100000 --data.way ${WAYS} --data.test_way ${WAYS} --train.learning_rate 0.0001 --data.cuda ${RELOAD_CMD}
--- centroid-networks/taskinfo/summarize.py
+++ centroid-networks/taskinfo/summarize.py
@@ -14,35 +14,40 @@ def smooth(y, box_pts):
     return y_smooth
 
 
+def int_or_none(x):
+    return int(x) if x else None
+
 parser = argparse.ArgumentParser()
-parser.add_argument('mode', type=str, choices=['plot', 'ls'])
 parser.add_argument('fname', type=str)
 parser.add_argument('-t', type=str, default='', help='variable to plot')
 parser.add_argument('-x', type=str, default='', help='x limits, e.g. -2,2')
 parser.add_argument('-y', type=str, default='', help='y limits, e.g. -2,2')
-parser.add_argument('-s', '--smooth', type=int, default=0, help='how much to smooth')
+parser.add_argument('-s', '--smooth', type=int, default=-1, help='how much to smooth')
 args = parser.parse_args()
 
 print('Reading {}'.format(args.fname))
 f = open(args.fname)
 d = json.load(f)
 
-if args.mode == 'ls':
-    for key in d:
+if args.t == '':
+    for key in sorted(d):
         print(key)
     sys.exit(-1)
 
-xlim = list(map(int, args.x.split(','))) if args.x else None
-ylim = list(map(int, args.y.split(','))) if args.y else None
+xlim = [int_or_none(i) for i in args.x.split(',')] if args.x else None
+ylim = [int_or_none(i) for i in args.y.split(',')] if args.y else None
 
 key = args.t
-sigma = (args.smooth // 2) *2 + 1
 
 data = sorted([(int(x), y) for x, y in d[key].items()])
 x, y = zip(*data)
 x = np.asarray(x)
 y = np.asarray(y)
-print('Plotting {} ({} samples, smooth {})...'.format(key, len(x), smooth))
 #smoothed = scipy.signal.medfilt(y, smooth)
+if args.smooth < 0:
+    args.smooth = len(x) // 20
+sigma = (args.smooth // 2) *2 + 1
+print('Plotting {} ({} samples, smooth {})...'.format(key, len(x), sigma))
 smoothed = smooth(y, sigma)
+
 gp.plot(x, smoothed, _with='lines', terminal='dumb 80, 40', unset='grid', title=key, _xrange=xlim, _yrange=ylim)
