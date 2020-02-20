1940c825de37dd9c3aee4ae45a065c7b74be451f
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-20 22:08:23 +0000

    fix evaluation code

--- centroid-networks/taskinfo/run_MODE_TRAINLOSS_DATASET_WAYS_SHOTS_RELOAD.sh
+++ centroid-networks/taskinfo/run_MODE_TRAINLOSS_DATASET_WAYS_SHOTS_RELOAD.sh
@@ -11,7 +11,7 @@ if [[ "$MODE" == "eval" ]]; then ACTUALTRAINLOSS="evalonly"; else ACTUALTRAINLOS
 
 PATH_TO_PRETRAINED="$HOME/code/cyvius96/save/proto-5/epoch-last.pth"
 if [ -z ${RELOAD+x} ]; then echo "please specify reload 1 or 0"; exit; fi
-if [ -n ${RELOAD} ]; then RELOAD_CMD="--checkpoint-state ${PATH_TO_PRETRAINED}"; fi
+if [ -n ${RELOAD} ]; then RELOAD_CMD="--checkpoint-state ${PATH_TO_PRETRAINED}" else RELOAD_CMD=""; fi
 
 # Replace this accordingly
 echo "CUDA visible devices $CUDA_VISIBLE_DEVICES"
