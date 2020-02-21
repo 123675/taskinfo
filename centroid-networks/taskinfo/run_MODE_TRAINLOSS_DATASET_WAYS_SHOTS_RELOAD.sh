47616b1ba53ba1e5e99527068ab23fe9b1d53211
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-21 00:38:55 +0000

    update script

--- centroid-networks/taskinfo/run_MODE_TRAINLOSS_DATASET_WAYS_SHOTS_RELOAD.sh
+++ centroid-networks/taskinfo/run_MODE_TRAINLOSS_DATASET_WAYS_SHOTS_RELOAD.sh
@@ -18,4 +18,4 @@ if [ ${RELOAD} == "1" ]; then RELOAD_CMD="--checkpoint-state ${PATH_TO_PRETRAINE
 echo "CUDA visible devices $CUDA_VISIBLE_DEVICES"
 set -o xtrace
 
-PYTHONPATH=.. python ../scripts/train/few_shot/run_train.py --data.dataset ${DATASET} --data.root $HOME/data/${DATASET} --model.model_name "pairwise_conv" --log.exp_dir results/${DATASET}-${WAYS}way-${SHOTS}shot-$TRAINLOSS --train-loss "${ACTUALTRAINLOSS}" --centroid-loss 0 --validate-interval 5 --iterations 100000 --data.way ${WAYS} --data.test_way ${WAYS} --train.learning_rate 0.0001 --data.cuda ${RELOAD_CMD}
+PYTHONPATH=.. python ../scripts/train/few_shot/run_train.py --data.dataset ${DATASET} --data.root $HOME/data/${DATASET} --model.model_name "pairwise_conv" --log.exp_dir results-${DATASET}-${WAYS}way-${SHOTS}shot/${DATASET}-${WAYS}way-${SHOTS}shot-$TRAINLOSS --train-loss "${ACTUALTRAINLOSS}" --centroid-loss 0 --validate-interval 5 --iterations 100000 --data.way ${WAYS} --data.test_way ${WAYS} --train.learning_rate 0.0001 --data.cuda ${RELOAD_CMD}
